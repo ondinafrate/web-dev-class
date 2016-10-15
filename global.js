ac9b86a2e64c247607c9fb93c922a7afc1181221
Ondina Frate <ondina@172-16-229-127.DYNAPOOL.NYU.EDU>
2016-10-15 16:48:39 -0400

    basic jquery

---
+++ global.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+
+
+		//BASIC EVENT HANDLER
+
+		$(".btn-success").on("mouseover", function(){
+
+			if($("header").hasClass("big")){
+
+				$("#header").removeClass("big")
+			}else{
+				$("#header").addClass("big")
+			}
+
+			$("ul li").each(function(){
+
+				//console.log("loop")
+
+				$(this).css({"color":"red", "font-weight": "bold", "font-size": "30px"})
+			})
+	
+		})
+
+		//JQuery
+		$(".btn-primary").on("click", function(){
+
+			var p = $("<p>Welcome</p>")
+			p.addClass("red")
+			$("dummy-text").append(p);
+
+		})
+
+		// JS
+		// var btn = document.getElementById("cl")
+
+		// btn.addEventListener("click", function(){
+
+		// 	var p = document.createElement("p")
+		// 	var text = document.createTextNode("welcome")
+
+		// 	p.appendChild(text)
+		// 	p.className = 'red'
+
+		// 	var bigpara = document.getElementById("append")
+		// 	bigpara.appendChild(p)
+
+		// })
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
